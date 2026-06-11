585069cf08290d6dd59718e2f19e3b1cf537d659
Yousef <yalmullaq8@gmail.com>
2026-06-11 21:37:17 +0300

    celery debug

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -8,3 +8,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+print("DEBUG CELERY_BROKER_URL:", os.environ.get("CELERY_BROKER_URL"))
+print("DEBUG REDIS_URL:", os.environ.get("REDIS_URL"))
